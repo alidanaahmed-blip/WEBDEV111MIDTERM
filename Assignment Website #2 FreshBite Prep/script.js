4a0144316ae2fef1b328c8aa2dd33d7096d0c64f
alidanaahmed-blip <alidanaahmed@gmail.com>
2026-03-26 14:41:59 +0800

    Add files via upload

---
+++ Assignment Website #2 FreshBite Prep/script.js
@@ -0,0 +1,17 @@
+
+function orderItem(itemName){
+alert("🥗 You ordered: " + itemName +
+"\n\nThank you for choosing FreshBite Prep!");
+}
+
+function sendMessage(){
+alert("🍊 Thank you for contacting FreshBite Prep! We'll reply soon.");
+}
+
+function openLogin(){
+document.getElementById("loginDrawer").style.right="0";
+}
+
+function closeLogin(){
+document.getElementById("loginDrawer").style.right="-350px";
+}
